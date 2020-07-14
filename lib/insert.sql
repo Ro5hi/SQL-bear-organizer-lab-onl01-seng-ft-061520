b91a5b85e804b1390dd23700f6bc0aeedba1351a
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 21:24:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 6, "F", "dark brown", "calm", )
+INSERT INTO bears VALUES (4, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
+INSERT INTO bears VALUES (5, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
+INSERT INTO bears VALUES (6, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
+INSERT INTO bears VALUES (7, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
+INSERT INTO bears VALUES (8, "Mr. Chocolate", 20, "M", "dark brown", "calm", )
