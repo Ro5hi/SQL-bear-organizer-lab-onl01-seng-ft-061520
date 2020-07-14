c05afe884c79c442f472bb3efd7b866287191da0
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 21:34:41 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,9 +23,9 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(bears.temperament) FROM bears where bears.temperament"
+  "SELECT COUNT(bears.temperament) FROM bears where bears.temperament='goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE bears.name IS NULL"
 end
